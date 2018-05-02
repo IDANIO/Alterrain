c8d6056b514f7fb90446df4eff1f65c1a8a28d8c
RaiderSoap <info@chivalrystudio.com>
2018-05-01 22:14:15 -0700

    miscellaneous

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+'use strict';
 const path = require('path');
+const compression = require('compression');
 
 const express = require('express');
 const app = express();
-
-const compression = require('compression');
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
 
+const logger = require('./app/logger.js');
+
 server.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
-  console.log(`http://localhost:${port}`);
+  logger.info(`Server listening at port ${port}`);
+  logger.info(`http://localhost:${port}`);
 });
 
 app.use(compression());
@@ -19,10 +21,11 @@ app.use(compression());
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 io.on('connection', (socket) => {
-  console.log('A client connected..');
+  logger.info('A client connected..');
 
   socket.on('disconnect', () => {
-    console.log('A client Disconnected..');
+    logger.info('A client Disconnected..');
   });
 });
